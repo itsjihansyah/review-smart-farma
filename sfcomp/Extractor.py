c5c56b2a8f3b6bef2d499da2cd1db98de02aac5d
Nabil Najmudin <nabiil.najm266@gmail.com>
2022-09-02 06:21:46 +0700

    adding cari_ket_diare func

--- sfcomp/Extractor.py
+++ sfcomp/Extractor.py
@@ -22,9 +22,9 @@ def extractor(s, med_rec):
         exist = med_rec[2]
 
     # dewasa-anak
-    if bool(re.search("dewasa", s)):
+    if bool(re.search("\s*de?wa?sa?", s, re.IGNORECASE)):
         exist.append("usia_dewasa")
-    elif bool(re.search("anak", s)):
+    elif bool(re.search("\s*(ana?k(\s*2|(-|\s*)\s*ana?k))", s, re.IGNORECASE)):
         exist.append("usia_anak")
     
     # demam
@@ -42,20 +42,33 @@ def extractor(s, med_rec):
         unexist.append("batuk")
 
     #diare 
-    if bool(re.search("\s*di?a?re",s)) :
+    if bool(re.search("\s*di?a?re",s, re.IGNORECASE)) :
         exist.append("diare")
-        ket_diare()
+        cari_ket_diare(s, exist, unexist)
 
     return [id, dialog_status, list(set(exist)), list(set(unexist))]
 
 
 # Function2 keterangan gejala 
-def ket_diare(message) :
-    print(print)
+# def ket_diare(message, exist, unexist) :
+#     print(print)
 
 # Function2 mencari keterangan gejala 
-def cari_ket_diare (message) : 
-    pattern = ["\s*(ta?nda?)?\s*((ke)?ga?wa?td?a?r?u?r?a?(ta?n)?)?\s*ABCD","\s*((sa?ki?t)|(nye?ri))\s*pe?ru?t\s*((pa?ra?h)|(he?ba?t)|(se?ka?li?))"]
+def cari_ket_diare (message, exist, unexist) : 
+    pattern = ["\s*(ta?nda?)?\s*((ke)?ga?wa?td?a?r?u?r?a?(ta?n)?)?\s*ABCD","\s*(((sa?ki?t)|(nye?ri))\s*pe?ru?t\s*((pa?ra?h)|(he?ba?t)|(se?ka?li?)|(kro?ni?s)|(ba?n?ge?t)))|((pe?ru?t)\s*((sa?ki?t)|(nye?ri))\s*((pa?ra?h)|(he?ba?t)|(se?ka?li?)|(kro?ni?s)|(ba?n?ge?t)))","\s*((obat)\s*(pemicu))|(dru?g\s*indu?ce?d\s*diarh?e?a)|(((obat)|(pil)|(kapsul)(tablet))\s*(\S)*)","\s*(((ti?nja?)|(fe?se?s)|(be?ra?k)|(bab))\s*((\w)*)?\s*(be?r)?da?ra?h)|(((\w)*)?\s*((\w)*)?\s*(be?r)?da?ra?h\s*((\w)*)?\s*((ti?nja?)|(fe?se?s)|(be?ra?k)|(bab)))|(((ti?nja?)|(fe?se?s)|(be?ra?k)|(bab))\s*(((\S)*)?\s*)*cu?ci?a?n\s*be?ra?s)|((((\S)*)?\s*)*cu?ci?a?n\s*be?ra?s)","\s*((\S|\s)*)+(de?ma?m|me?ri?a?ng|ge?ra?h|pa?na?s|palak)","\s*((\S|\s)*)+(mual|muntah)","\s*(((\S|\s)*)+(dahaga|haus)\s*(se?kali|ba?nge?t|pa?ra?h))|(urine?((\w|\s)*)?ge?la?p)|(ge?la?p\s*((\w)*)?\s*urine?((\w)*)?)|(ku?li?t((\w|\s)*)?(ke?ri?ng|ga?ri?ng))"]
+
+    ket_gejala = ["darurat ABCD","diare > 14 hari","nyeri perut hebat","ada drug induced diarhea","tinja berdarah/spt cucian beras","demam","mual muntah","dehidrasi"]
+
+    i = 0
+    while (i <= len(pattern) -1 ) : 
+        if(bool(re.search(pattern[i],message,re.IGNORECASE))) : 
+            exist.append(ket_gejala[i])
+        else : 
+            unexist.append(ket_gejala[i])
+        
+        i += 1
+
+    
 
 
 
